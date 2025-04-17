457b044bc6639340dc51f92919f0ac141f27677f
Jeff Verdu <jeffverdu@gmail.com>
2025-04-17 14:29:33 -0400

    Update Axios base URL to use production endpoint and clean up request interceptor

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,17 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://proyecto-final-0dd6.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Interceptor para agregar token a cada request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // rutas protegidas
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
